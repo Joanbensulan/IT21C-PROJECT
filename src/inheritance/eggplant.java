58f34da07dea82ca67ab690c8cad12477178e127
Joanbensulan <joanbensulan2@gmail.com>
2023-11-17 14:16:40 +0800

    added inheritance class, eggplant class, vegetables class and inheritance package

---
+++ src/inheritance/eggplant.java
@@ -0,0 +1,13 @@
+package inheritance;
+
+public class eggplant extends vegetables {
+
+    public eggplant(String utan) {
+        super(utan);
+    }
+
+    void daut() {
+        System.out.println(utan + " is lata man uy");
+    }
+
+}
