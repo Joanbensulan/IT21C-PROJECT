823914e09c4dcd052d234e0a6d156de2765912ae
Joanbensulan <joanbensulan2@gmail.com>
2023-11-17 14:34:40 +0800

    Added student class on student package

---
+++ src/Student/Student.java
@@ -0,0 +1,23 @@
+  package Student;
+          
+class Student {
+      
+    String name;
+    String section;
+    String motto;
+
+    public Student(String name, String section, String motto) {
+        this.name = name;
+        this.section = section;
+        this.motto = motto;
+    }
+
+    public void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Section: " + section);
+        System.out.println("Motto: " + motto);
+    
+}
+  }
+
+    
